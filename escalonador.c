ca8c55c8470f79c1ad0453c5a4da4feda24e21bf
Gyselle Mello <gysellemello@id.uff.br>
2021-08-03 15:48:50 -0300

    Commit inicial com as estruturas que serão utilizadas

---
+++ escalonador.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Processo{
+    int numero_processo;
+    int tempo_de_chegada;
+    int prioridade;
+    int tempo_de_processador;
+    int memoria;
+    int unidade_disco;
+    struct Processo *prox;
+}TProcesso;
+
+typedef struct Recursos{
+    int cpu1;
+    int cpu2;
+    int cpu3;
+    int cpu4;
+    int memoria;
+    int unidade_disco;
+}TRecursos;
+
+typedef struct fila {
+    TProcesso *inicio;
+    TProcesso *fim;
+}TFila;
