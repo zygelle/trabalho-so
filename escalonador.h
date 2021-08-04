259336ddc4859066186d6220f57696b775ad6ae3
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-04 18:26:00 -0300

    moved structs to header file

--- escalonador.h
+++ escalonador.h
@@ -1,6 +1,29 @@
 #ifndef ESCALONADOR_H_
 #define ESCALONADOR_H_
 
+typedef struct Processo {
+    int numero_processo;
+    int tempo_de_chegada;
+    int prioridade;
+    int tempo_de_processador;
+    int memoria;
+    int unidade_disco;
+    struct Processo *prox;
+} TProcesso;
+
+typedef struct Recursos {
+    int cpu1;
+    int cpu2;
+    int cpu3;
+    int cpu4;
+    int memoria;
+    int unidade_disco;
+} TRecursos;
+
+typedef struct Fila {
+    TProcesso *inicio;
+    TProcesso *fim;
+} TFila;
 
 
 #endif // ESCALONADOR_H_
