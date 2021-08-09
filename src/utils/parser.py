62bd1d2df94ecf3e756167aee53a76939419ed5d
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-08 21:01:15 -0300

    parser retorna agora ordenado

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -4,11 +4,12 @@ from classes.Processo import Processo
 def txtParaListaDeProcessos(caminho: str) -> list[Processo]:
     arquivo = open(caminho, "r")
     linhas = arquivo.readlines()
-    listaProcessos: list[Processo] = []
+    lista_processos: list[Processo] = []
     pid = 0
     for linha in linhas:
         args = list(map(int, linha.split(", ")))
         processo = Processo(pid, args[0], args[1], args[2], args[3], args[4])
-        listaProcessos.append(processo)
+        lista_processos.append(processo)
         pid += 1
-    return listaProcessos
+
+    return sorted(lista_processos, key=lambda i: i.tempo_de_chegada)
