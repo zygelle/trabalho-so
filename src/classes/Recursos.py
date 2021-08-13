50b7e8052caf27efab8047b5692026e98e8db76e
RodrigoGCorrea <rodrigogc@id.uff.br>
2021-08-12 23:46:31 -0300

    mudando nome de função e implementando checagem

--- src/classes/Recursos.py
+++ src/classes/Recursos.py
@@ -45,8 +45,13 @@ class Recursos:
         self.executando.append(processo)
         return True
 
-    def executa(self):
+    def processa(self):
         for i in range(len(self.executando) - 1, -1, -1):
             self.executando[i].passarTempo(self)
-            if (self.executando[i].tempo_de_processador == -1):
+            if self.executando[i].tempo_de_processador == -1:
                 self.executando.pop(i)
+
+    def checaPossibilidadeAlocaDisco(self, processo):
+        if self.discos - processo.unidade_disco >= 0:
+            return True
+        return False
