f7d0aa015c05fb75a4a47d9483c0377f9ba2520f
RodrigoGCorrea <rodrigogc@id.uff.br>
2021-08-12 23:44:12 -0300

    Mudanças em adiciona_processo

--- src/classes/Fcfs.py
+++ src/classes/Fcfs.py
@@ -10,27 +10,28 @@ class Fcfs:
     fila: list[Processo] = field(default_factory=list)
     bloqueado_memoria: list[Processo] = field(default_factory=list)
 
-    def adiciona_processo(self, processo: list[Processo], recursos: Recursos, feedback: Feedback) -> None:
+    def adiciona_processo(
+        self, processo: list[Processo], recursos: Recursos, feedback: Feedback
+    ) -> None:
         for p in processo:
-            while(not p.aloca(recursos)):
-                if (len(feedback.fila2) > 0):
+            while not p.aloca(recursos):
+                if len(feedback.fila2) > 0:
                     processo = feedback.fila2.pop(len(feedback.fila2))
                     processo.libera()
                     feedback.bloqueia_processo_memoria(processo)
-                elif (len(feedback.fila1) > 0):
+                elif len(feedback.fila1) > 0:
                     processo = feedback.fila1.pop(len(feedback.fila1))
                     processo.libera()
                     feedback.bloqueia_processo_memoria(processo)
-                elif (len(feedback.fila0) > 0):
+                elif len(feedback.fila0) > 0:
                     processo = feedback.fila0.pop(len(feedback.fila0))
                     processo.libera()
                     feedback.bloqueia_processo_memoria(processo)
                 else:
                     self.bloqueado_memoria.append(p)
                     break
-            if (p not in self.bloqueado_memoria):
+            if p not in self.bloqueado_memoria:
                 self.fila.append(p)
-
 
     def processa(self, recursos: Recursos, feedback: Feedback):
         if not self.fila:
@@ -44,17 +45,17 @@ class Fcfs:
                 self.fila[0].executa(recursos)
                 recursos.executando.append(self.fila.pop(0))
         else:
-            p1 = []
+            p1:list[Processo] = []
             for i in range(recursos.max_cpus):
                 atual_exec = recursos.executando[i]
                 if atual_exec.prioridade == 1:
                     p1.append(i)
 
-            for i in p1:
+            for processo in p1:
                 if not self.fila:
                     return
 
-                feedback.para_processo(p1)
-                recursos.executando.remove(p1)
+                feedback.para_processo(processo, recursos)
+                recursos.executando.remove(processo)
                 self.fila[0].executa(recursos)
                 recursos.executando.append(self.fila.pop(0))
