ba1ac345774e835c7219ce5ebeab46fe5fdf0878
RodrigoGCorrea <rodrigogc@id.uff.br>
2021-08-12 23:45:11 -0300

    mudanças adiciona_processo implementação processa

--- src/classes/Feedback.py
+++ src/classes/Feedback.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from .Processo import Processo
 from .Recursos import Recursos
 
+
 @dataclass(init=True)
 class Feedback:
     fila0: list[Processo] = field(default_factory=list)
@@ -13,28 +14,70 @@ class Feedback:
     proxFila2: list[int] = field(default_factory=list)
     quantum: int = 2
 
-    def adiciona_processo(self, processo: list[Processo], recursos: Recursos) -> None:
-        for p in processo:
-            if (p.aloca(recursos)):
-                self.fila0.append(p)
-                self.proxFila1.append(p.numero_processo)
+    def adiciona_processo(self, processos: list[Processo], recursos: Recursos) -> None:
+        for processo in processos:
+            if processo.aloca(recursos):
+                self.fila0.append(processo)
+                self.proxFila1.append(processo.numero_processo)
             else:
-                self.bloqueado_memoria.append(p)
-    
+                self.bloqueado_memoria.append(processo)
+
     def para_processo(self, processo: Processo, recursos: Recursos) -> None:
         processo.bloqueia(recursos)
-        if (processo.numero_processo in self.proxFila1):
+        if processo.numero_processo in self.proxFila1:
+            self.proxFila1.remove(processo.numero_processo)
             self.fila1.append(processo)
-        elif (processo.numero_processo in self.proxFila2):
+            self.proxFila2.append(processo.numero_processo)
+        elif processo.numero_processo in self.proxFila2:
+            self.proxFila2.remove(processo.numero_processo)
             self.fila2.append(processo)
         else:
             self.fila0.append(processo)
-    
+            self.proxFila1.append(processo.numero_processo)
+
     def bloqueia_processo_memoria(self, processo: Processo) -> None:
         self.bloqueado_memoria.append(processo)
-    
+
     def bloqueia_processo_disco(self, processo: Processo) -> None:
         self.bloqueado_disco.append(processo)
 
-    def processa(recursos: Recursos) -> None:
-        pass
+    def insere_na_fila_certa(
+        self, fila: list[Processo], recursos: Recursos, momento_atual: int
+    ) -> None:
+        if recursos.checaPossibilidadeAlocaDisco(fila[0]):
+            processo = fila.pop(0)
+            processo.executa(recursos)
+            processo.inicio_execucao = momento_atual
+            recursos.executando.append(processo)
+        else:
+            self.bloqueia_processo_disco(fila.pop(0))
+
+    def processa(self, recursos: Recursos, momento_atual: int) -> None:
+        if len(self.fila0) == 0 and len(self.fila1) == 0 and len(self.fila2) == 0:
+            return
+
+        if recursos.cpus > 0:
+            for _ in range(recursos.cpus):
+                if len(self.fila0) > 0:
+                    self.insere_na_fila_certa(self.fila0, recursos, momento_atual)
+                elif len(self.fila1) > 0:
+                    self.insere_na_fila_certa(self.fila1, recursos, momento_atual)
+                elif len(self.fila2) > 0:
+                    self.insere_na_fila_certa(self.fila2, recursos, momento_atual)
+        else:
+            p1: list[Processo] = []
+            for i in range(recursos.max_cpus):
+                atual_exec = recursos.executando[i]
+                if atual_exec.prioridade == 1:
+                    p1.append(i)
+
+            for processo in p1:
+                if momento_atual > processo.inicio_execucao + self.quantum:
+                    self.para_processo(processo)
+                    recursos.executando.remove(processo)
+                    if len(self.fila0) > 0:
+                        self.insere_na_fila_certa(self.fila0, recursos, momento_atual)
+                    elif len(self.fila1) > 0:
+                        self.insere_na_fila_certa(self.fila1, recursos, momento_atual)
+                    elif len(self.fila2) > 0:
+                        self.insere_na_fila_certa(self.fila2, recursos, momento_atual)
